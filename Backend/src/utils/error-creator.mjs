0601b842caa408200820ea13da018099e5893c44
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2026-05-14 18:40:22 +0530

    initial commit

--- Backend/src/utils/error-creator.mjs
+++
@@ -1,7 +0,0 @@
-export const errorCreate = (errors) => {
-   let extractedErrors = {};
-   errors.forEach((err) => {
-      extractedErrors[err.path] = err.msg;
-   });
-   return extractedErrors;
-};
--- Frontend/src/utils/Values.jsx
+++
@@ -1,2 +0,0 @@
-export const CONTACT_ADMIN_EMAIL = "mailto:navodyadushan@gmail.com";
-export const SUPPORT_PHONE = "+94 71 123 4567";
